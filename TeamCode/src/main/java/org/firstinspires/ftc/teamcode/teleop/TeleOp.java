cd18b0bef5dcce0fae1e5eae284215ba91309c49
DaniNayal <dani@nayal.org>
2024-11-02 14:46:46 -0400

    added to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -6,16 +6,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
         double hangTarget = 0;
-        double bucketTarget = 0;
+        double extendoPitchTarget = 0;
+        double extendoTarget = 0;
+        double bucketSlidesTarget = 0;
+        double clawWristPosition = 0;
+
+        boolean isPressingA = false;
+        boolean isPressingY2 = false;
+        boolean isPressingA2 = false;
 
         double kP = 0.015;
 
@@ -28,99 +33,162 @@ public class TeleOp extends LinearOpMode {
         DcMotor hang = hardwareMap.dcMotor.get("hang");
         DcMotor bucketSlides = hardwareMap.dcMotor.get("bucket");
 
-        Servo clawPitchRight = hardwareMap.servo.get("clawPitchRight");
         Servo clawPitchLeft = hardwareMap.servo.get("clawPitchLeft");
+        Servo clawPitchRight = hardwareMap.servo.get("clawPitchRight");
         Servo clawFingers = hardwareMap.servo.get("clawFingers");
         Servo clawWrist = hardwareMap.servo.get("clawWrist");
         Servo bucket = hardwareMap.servo.get("bucket");
 
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+
         extendo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Test Reverse
+        extendo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        extendoPitch.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Test Reverse
+        extendoPitch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        hang.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hang.setDirection(DcMotorSimple.Direction.REVERSE);
+        hang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         bucketSlides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bucketSlides.setDirection(DcMotor.Direction.REVERSE);
         bucketSlides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        clawPitchLeft.setDirection(Servo.Direction.REVERSE);
+        // Test clawPitchLeft Reverse
+        // Test clawPitchRight Reverse
+        // Test clawFingers Reverse
+        // Test clawWrist Reverse
+        // Test bucket Reverse
 
-
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        // Retrieve the IMU from the hardware map
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
         imu.initialize(parameters);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        boolean isPressing2A=false;
-        boolean isPressingA=false;
-
         while (opModeIsActive()) {
 
+            // Claw pitch to position 0 to 1
+            if (gamepad2.left_bumper){
+                clawPitchLeft.setPosition(0);
+                clawPitchRight.setPosition(0);
+            }
+            if (gamepad2.right_bumper){
+                clawPitchLeft.setPosition(1);
+                clawPitchRight.setPosition(1);
+            }
+            telemetry.addData("left claw pitch position", clawPitchLeft.getPosition());
+            telemetry.addData("right claw pitch position", clawPitchRight.getPosition());
+
             // Hang toggle between min and max positions
-            if (gamepad2.a){
-                if (!isPressing2A) {
-                    if (hangTarget==0) {hangTarget=3634;} else {hangTarget=0;}
-                    isPressing2A=true;
+            if (gamepad2.y){
+                if (!isPressingY2) {
+                    if (hangTarget == 0) {
+                        isPressingY2 = true;
+                        hangTarget = 3634;}
+                    else hangTarget = 0;
+
                 }
             }
-            else{
-                isPressing2A=false;
+            else {
+                isPressingY2=false;
             }
-            hang.setPower(-(hang.getCurrentPosition()-hangTarget) * kP);
+            hang.setPower((hangTarget = hang.getCurrentPosition()) * kP);
             telemetry.addData("hang pos", hang.getCurrentPosition());
             telemetry.addData("hang target", hangTarget);
 
             // Bucket Slides toggle between min and max positions
             if (gamepad1.a){
                 if (!isPressingA) {
-                    if (bucketTarget==0) {bucketTarget=3634;} else {bucketTarget=0;}
-                    isPressingA=true;
+                    if (bucketSlidesTarget == 0) {
+                        isPressingA=true;
+                        bucketSlidesTarget = 1891;}
+                    else bucketSlidesTarget = 0;
+
                 }
             }
             else{
                 isPressingA=false;
             }
-
-            bucketSlides.setPower(-(bucketSlides.getCurrentPosition()-bucketTarget) * kP);
+            bucketSlides.setPower((bucketSlidesTarget - bucketSlides.getCurrentPosition()) * kP);
             telemetry.addData("bucket pos", bucketSlides.getCurrentPosition());
-            telemetry.addData("bucket target", bucketTarget);
+            telemetry.addData("bucket target", bucketSlidesTarget);
 
-            if (gamepad1.x){
-
+            // When bucket slides are going up the bucket will move when the slides are 100 ticks away from max position
+            if (bucketSlidesTarget - bucketSlides.getCurrentPosition()<100){
+                if (bucketSlidesTarget==1891){
+                    bucket.setPosition(1);
+                }
+                else{
+                    bucket.setPosition(0.2);
+                }
             }
-            if (gamepad1.y){
-
-            }
-            if (gamepad1.right_bumper){
-
-            }
+            // Dynamic extendo control
             if (gamepad1.left_bumper){
-
+                extendoTarget += 15;
             }
-            if (gamepad1.left_trigger > 0.5){
-
+            else if (gamepad1.right_bumper){
+                extendoTarget -= 15;
             }
-            if (gamepad1.right_trigger > 0.5){
+            extendo.setPower((extendoTarget - extendo.getCurrentPosition()) * kP);
+            telemetry.addData("extendo position", extendo.getCurrentPosition());
+            telemetry.addData("extendo target", extendoTarget);
 
+            // Extendo pitch presets
+            if (gamepad1.a){
+                // extendoPitchTarget = down position
+            }
+            if (gamepad1.x){
+                // extendoPitchTarget = default / intake position
+            }
+            extendoPitch.setPower((extendoPitchTarget - extendo.getCurrentPosition()) * kP);
+            telemetry.addData("extendo pitch position", extendoPitch.getCurrentPosition());
+            telemetry.addData("extendo pitch target", extendoPitchTarget);
+
+            // Claw fingers toggle between open and closed
+            if (gamepad2.a) {
+                if (!isPressingA2) {
+                    if (clawFingers.getPosition() == 0.8) {
+                        isPressingA2 = true;
+                        clawFingers.setPosition(1);
+                    }
+                    else clawFingers.setPosition(0.8);
+                }
+            }
+            else isPressingA2 = false;
+            telemetry.addData("claw finger position", clawFingers.getPosition());
+
+            // Claw wrist dynamic movement
+            if (gamepad2.left_bumper && clawWrist.getPosition() > 0){
+                clawWristPosition -= 0.01;
+            }
+            if (gamepad2.right_bumper && clawWrist.getPosition() < 1){
+                clawWristPosition += 0.01;
+            }
+            clawWrist.setPosition(clawWristPosition);
+            telemetry.addData("claw wrist position", clawWrist.getPosition());
+
+
+
+
+
+            if (gamepad1.options) {
+                imu.resetYaw();
             }
 
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
-
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
