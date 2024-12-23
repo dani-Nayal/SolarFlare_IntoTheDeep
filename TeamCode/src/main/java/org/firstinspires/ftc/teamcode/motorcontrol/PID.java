ba5b263b1f5b6225d710c520cb2db3a45689a6f5
DaniNayal <dani.nayal@icloud.com>
2024-12-22 20:26:18 -0500

    removed integralsum limit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorcontrol/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorcontrol/PID.java
@@ -12,7 +12,6 @@ public class PID {
     int lastError = 0;
     int lastReference = 0;
     double integralSum = 0;
-    double integralSumLimit = 0.25;
     ElapsedTime timer = new ElapsedTime();
     public PID(){
         hw = HardwareConfig.getHardwareConfig();
@@ -31,12 +30,7 @@ public class PID {
         if (reference != lastReference){
             integralSum = 0;
         }
-        if (integralSum > integralSumLimit){
-            integralSum = integralSumLimit;
-        }
-        if (integralSum < -integralSumLimit){
-            integralSum = -integralSumLimit;
-        }
+        
         double proportionalPower = error * hw.getMotorConfig(motorEnum).kP;
         double integralPower = integralSum * hw.getMotorConfig(motorEnum).kI;
         double derivativePower = derivative * hw.getMotorConfig(motorEnum).kD;
