3f90f1ffbc76a7fa90b4ccde07ec5c8538879f12
dani-Nayal <dani.nayal@icloud.com>
2024-12-28 14:33:31 -0800

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoSubmersibleCycle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoSubmersibleCycle.java
@@ -11,6 +11,15 @@ import java.util.List;
 
 @Autonomous
 public class AutoSubmersibleCycle extends LinearOpMode {
+
+    // First, position the robot in the optimal pickup position for the sample using tx and ty
+    // Next, find which detection is the target result by comparing result tx to detection tx
+    // When we know which detection is the target, find the corners of the detection
+    // Find the longer side of the target sample detection box (detection box is never rotated)
+    // If longer side is on the top / bottom of the rectangle, rotate clawWrist to x position
+    // If longer side is on left / right side of the rectagle, rotate clawWrist to y position
+    // If the sides are =, rotate clawWrist to y position
+
     // top left is 0,0
     int LONG_SIDE_PIXELS = 680;
     int SHORT_SIDE_PIXELS = 480;
@@ -28,10 +37,17 @@ public class AutoSubmersibleCycle extends LinearOpMode {
         while (opModeIsActive()) {
             LLResult result = hw.getLimelightConfig().limelight.getLatestResult();
 
-            telemetry.addData("x", result.getTx());
-            telemetry.addData("y", result.getTy());
-            telemetry.addData("area percent", result.getTa());
-            telemetry.addData("sample type", result.getClass());
+            if (result.isValid()){
+                double x = result.getTx();
+                double y = result.getTy();
+                double area = result.getTa();
+
+
+
+                telemetry.addData("x", x);
+                telemetry.addData("y", y);
+                telemetry.addData("area", area);
+            }
             telemetry.update();
         }
     }
