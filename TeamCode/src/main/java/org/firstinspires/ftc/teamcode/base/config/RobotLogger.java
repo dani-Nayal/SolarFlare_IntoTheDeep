6be8fee040e6c46488b5101e84f16e17c8dd364a
MuradGitHub <github@nayal.org>
2025-01-15 13:08:19 -0500

    Move the initialization of robot.config to a static block

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/config/RobotLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/config/RobotLogger.java
@@ -4,27 +4,28 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.StreamHandler;
 
 public class RobotLogger {
-    private static Logger configLogger = null;
+    public static Logger configLogger;
+
+    static {
+        ConsoleHandler handler = new ConsoleHandler() {
+            @Override
+            public synchronized void publish(final LogRecord record) {
+                super.publish(record);
+                flush();
+            }
+        };
+        handler.se
+        handler.setLevel(Level.ALL);
+
+        configLogger = Logger.getLogger("robot.config");
+        configLogger.setLevel(Level.ALL);
+        configLogger.setUseParentHandlers(false);
+        configLogger.addHandler(handler);
+    }
 
     public static Logger getConfigLogger() {
-        if(configLogger == null) {
-            ConsoleHandler handler = new ConsoleHandler() {
-                @Override
-                public synchronized void publish(final LogRecord record) {
-                    super.publish(record);
-                    flush();
-                }
-            };
-            handler.setLevel(Level.ALL);
-
-            configLogger = Logger.getLogger("robot.config");
-            configLogger.setLevel(Level.ALL);
-            configLogger.setUseParentHandlers(false);
-            configLogger.addHandler(handler);
-        }
         return configLogger;
     }
 }
