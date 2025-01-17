effd6b5b68bd24327f3714b384ac752ffad02094
MuradGitHub <github@nayal.org>
2025-01-17 16:56:54 -0500

    Update SimpleMotorTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/testing/SimpleMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/testing/SimpleMotorTest.java
@@ -2,28 +2,68 @@ package org.firstinspires.ftc.teamcode.base.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.base.config.HardwareConfig;
+import org.firstinspires.ftc.teamcode.base.config.MotorConfig;
+import org.firstinspires.ftc.teamcode.base.config.MotorEnum;
+import org.firstinspires.ftc.teamcode.base.config.RobotConfig;
+import org.json.JSONException;
 
 @TeleOp
 public class SimpleMotorTest extends LinearOpMode {
     public void runOpMode() {
-        DcMotor motor = hardwareMap.dcMotor.get("motor-1");
+        RobotConfig robotConfig = null;
+        HardwareConfig hw = null;
+        try {
+            robotConfig = RobotConfig.createInstance("Rig1Motor");
+            hw = HardwareConfig.createInstance(hardwareMap, robotConfig);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        MotorConfig motorConfig = hw.getMotorConfig(MotorEnum.TESTING_MOTOR);
+        DcMotorEx      motor    = motorConfig.motor;
+        DcMotorEx motor_direct  = hardwareMap.get(DcMotorEx.class, "motor-1");
+
+        telemetry.addData("reference equality", motor == motor_direct);
+        telemetry.addData("value equality", motor.equals(motor_direct));
+        telemetry.addData("SimpleMotorTest", "Done with initialization");
+        telemetry.update();
 
         waitForStart();
 
+        double motorPower = 0;
         while (opModeIsActive()) {
             if (gamepad1.a){
-                motor.setPower(0);
+                motorPower = 0;
+                telemetry.addData("setPower=", motorPower);
+                motor.setPower(motorPower);
+                motor_direct.setPower(motorPower);
             }
-            else if (gamepad1.b){
-                motor.setPower(0.25);
+            else if (gamepad1.b) {
+                motorPower = 0.33;
+                telemetry.addData("setPower=", motorPower);
+                motor.setPower(motorPower);
+                //motor_direct.setPower(motorPower);
             }
-            else if (gamepad1.y){
-                motor.setPower(0.75);
+            else if (gamepad1.y) {
+                motorPower = 0.66;
+                telemetry.addData("setPower=", motorPower);
+                motor.setPower(motorPower);
+                //motor_direct.setPower(motorPower);
             }
             else if (gamepad1.x){
-                motor.setPower(1.0);
+                motorPower = 1.0;
+                telemetry.addData("setPower=", motorPower);
+                motor.setPower(motorPower);
+                //motor_direct.setPower(motorPower);
+            } else {
+                motorPower = 0;
+                telemetry.addData("SimpleMotorTest", "Nothing is pressed. Zero Power");
+                motor.setPower(motorPower);
+                //motor_direct.setPower(motorPower);
             }
+            telemetry.update();
         }
     }
 }
