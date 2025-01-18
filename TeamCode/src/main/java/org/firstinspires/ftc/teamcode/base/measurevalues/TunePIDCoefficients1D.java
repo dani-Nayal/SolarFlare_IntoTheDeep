a7e6a7443d768d957c366f33f599b865b992d09c
MuradGitHub <github@nayal.org>
2025-01-18 08:29:47 -0500

    Create TunePIDCoefficients1D.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/measurevalues/TunePIDCoefficients1D.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode.base.measurevalues;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.base.config.HardwareConfig;
+import org.firstinspires.ftc.teamcode.base.config.MotorConfig;
+import org.firstinspires.ftc.teamcode.base.config.MotorEnum;
+import org.firstinspires.ftc.teamcode.base.config.RobotConfig;
+import org.firstinspires.ftc.teamcode.base.config.RobotState;
+import org.json.JSONException;
+
+import java.util.Locale;
+
+@Config
+@TeleOp
+public class TunePIDCoefficients1D extends LinearOpMode {
+    static final MotorEnum motorEnum = MotorEnum.TESTING_MOTOR;
+    static final int       errorTol  = 5;
+    RobotConfig            robotConfig;
+    HardwareConfig         hardwareConfig;
+    RobotState             state;
+    MotorConfig            motorConfig;
+    DcMotorEx              motor;
+
+    public static double kP              = 0.0335; // original
+    public static double kI              = 0.0;
+    public static double kD              = 0.0; // original
+    // 0.0335
+
+    @Override
+    public void runOpMode(){
+        try {
+            robotConfig    = RobotConfig.createInstance("Rig1Motor");
+            hardwareConfig = HardwareConfig.createInstance(hardwareMap, robotConfig);
+            motorConfig    = hardwareConfig.getMotorConfig(motorEnum);
+            motor          = motorConfig.motor;
+            state          = RobotState.getInstance();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Telemetry    telemetry = dashboard.getTelemetry();
+        ElapsedTime  timer     = new ElapsedTime();
+
+
+        // double lastPosition    = 0;
+        double lastTarget      = 0;
+        double integralSum     = 0;
+
+        waitForStart();
+
+        boolean convFlag       = false;
+        double convTime        = Double.POSITIVE_INFINITY;
+        ElapsedTime convTimer  = new ElapsedTime();
+        convTimer.reset();
+
+        int iteration          = 0;
+        while (opModeIsActive()){
+            if (gamepad1.a){
+                state.setMotorTarget(motorEnum, (int) (motorConfig.maxTarget * 0.25));
+            }
+            else if (gamepad1.b){
+                state.setMotorTarget(motorEnum, (int) (motorConfig.maxTarget * 0.5));
+            }
+            else if (gamepad1.y){
+                state.setMotorTarget(motorEnum, (int) (motorConfig.maxTarget * 0.75));
+            }
+            else if (gamepad1.x){
+                state.setMotorTarget(motorEnum, motorConfig.maxTarget);
+            }
+
+            double currentTarget     = state.getMotorTarget(motorEnum);
+            double currentPosition   = motor.getCurrentPosition();
+            double dt                = timer.seconds();
+            double error             = currentTarget - currentPosition;
+            // double derivative        = (currentPosition - lastPosition) / dt;
+            double derivative        = motor.getVelocity();
+
+            if (currentTarget != lastTarget) {
+                convFlag             = false;
+                integralSum          = 0;
+                convTimer.reset();
+            }
+            integralSum              = integralSum + (error * dt);
+
+            if(abs(error) < errorTol && !convFlag) {
+                convFlag             = true;
+                convTime             = convTimer.seconds();
+            }
+
+            double proportionalPower = max(min(error * kP,1),-1);
+            double integralPower     = max(min(integralSum * kI,1),-1);
+            double derivativePower   = max(min(derivative * kD,1),-1);
+            double outPower          = max(min(proportionalPower+integralPower+derivativePower,1),-1);
+
+            motor.setPower(outPower);
+
+            lastTarget               = currentTarget;
+          //  lastPosition             = currentPosition;
+
+            timer.reset();
+
+            telemetry.addData("Iteration", iteration++);
+            telemetry.addData("K- Kp", String.format(Locale.US,"%1$15.8f", kP));
+            telemetry.addData("K- Kd", String.format(Locale.US,"%1$15.8f", kD));
+            telemetry.addData("K- Ki", String.format(Locale.US,"%1$15.8f", kI));
+            telemetry.addData("P- convergence", convFlag);
+            telemetry.addData("P- convergence time", convTime);
+            telemetry.addData("P- motor max target", motorConfig.maxTarget);
+            telemetry.addData("P- motor target", state.getMotorTarget(motorEnum));
+            telemetry.addData("P- motor position", hardwareConfig.getMotorConfig(motorEnum).motor.getCurrentPosition());
+            telemetry.addData("P- error", error);
+            telemetry.addData("R- proportionalPower", proportionalPower);
+            telemetry.addData("R- derivative", derivative);
+            telemetry.addData("R- derivativePower", derivativePower);
+            telemetry.addData("R- integralSum", integralSum);
+            telemetry.addData("R- integralPower", integralPower);
+            telemetry.addData("R- motor power", outPower);
+            telemetry.update();
+        }
+    }
+}
