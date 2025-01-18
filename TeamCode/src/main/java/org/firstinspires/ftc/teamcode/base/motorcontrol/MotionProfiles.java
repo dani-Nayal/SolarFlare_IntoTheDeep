aac5daa79fb886ccc0ef00a722d4d8f16f2c48fd
MuradGitHub <github@nayal.org>
2025-01-18 08:31:01 -0500

    Create MotionProfiles.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/motorcontrol/MotionProfiles.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.base.motorcontrol;
+
+public class MotionProfiles {
+    public double runTrapezoidalMotionProfile(double maxMotorVelocity, double maxMotorAcceleration, double distance, double elapsedTime){
+        double accelerationDT = maxMotorVelocity / maxMotorAcceleration;
+        double halfWayDistance = distance / 2;
+        double accelerationDistance = 0.5 * maxMotorAcceleration * Math.pow(accelerationDT, 2);
+
+        if (Math.abs(accelerationDistance) > Math.abs(halfWayDistance)){
+            accelerationDT = Math.sqrt(halfWayDistance / (0.5 * maxMotorAcceleration));
+        }
+        accelerationDistance = 0.5 * maxMotorAcceleration * Math.pow(accelerationDT, 2);
+
+        maxMotorVelocity = maxMotorAcceleration * accelerationDT;
+
+        double decelerationDT = accelerationDT;
+
+        double cruiseDistance = distance - 2 * accelerationDistance;
+        double cruiseDT = cruiseDistance / maxMotorVelocity;
+        double accelerateAndCruiseDT = accelerationDT + cruiseDT;
+
+        double entireDT = accelerationDT + cruiseDT + decelerationDT;
+        if (elapsedTime > entireDT){
+            return distance;
+        }
+
+        if (elapsedTime < accelerationDT){
+            return 0.5 * maxMotorAcceleration * Math.pow(elapsedTime, 2);
+        }
+
+        else if (elapsedTime < accelerateAndCruiseDT){
+            accelerationDistance = 0.5 * maxMotorAcceleration * Math.pow(accelerationDT, 2);
+            double cruiseCurrentDT = elapsedTime - accelerationDT;
+            return accelerationDistance + maxMotorVelocity * cruiseCurrentDT;
+        }
+
+        else {
+            accelerationDistance = 0.5 * maxMotorAcceleration * Math.pow(accelerationDT, 2);
+            cruiseDistance = maxMotorVelocity * cruiseDT;
+            return accelerationDistance + cruiseDistance + maxMotorVelocity * cruiseDT - 0.5 * maxMotorAcceleration * Math.pow(decelerationDT, 2);
+        }
+    }
+}
