0e095c4bcda321cdcd0001443c013ae43779b6ab
MuradGitHub <github@nayal.org>
2025-01-18 08:31:26 -0500

    Update Test1Motor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/testing/Test1Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/testing/Test1Motor.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.base.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.base.config.HardwareConfig;
 import org.firstinspires.ftc.teamcode.base.config.MotorEnum;
@@ -27,6 +28,9 @@ public class Test1Motor extends LinearOpMode {
         state = RobotState.getInstance();
         motorControl = new MotorControl(MotorEnum.TESTING_MOTOR);
 
+
+        hw.getMotorConfig(MotorEnum.TESTING_MOTOR).motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hw.getMotorConfig(MotorEnum.TESTING_MOTOR).motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         waitForStart();
 
         while (opModeIsActive()){
@@ -43,7 +47,7 @@ public class Test1Motor extends LinearOpMode {
                 state.setMotorTarget(MotorEnum.TESTING_MOTOR, 1819);
             }
 
-            motorControl.runOldTrapezoidalMotionProfile();
+            motorControl.runPIDMotorControl();
         }
     }
 }
