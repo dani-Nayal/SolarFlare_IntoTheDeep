804e797e84e674a05f5413e8b0d179d6c618c3e6
roobootics <arick@solarflarerobotics.org>
2025-01-22 19:27:44 -0500

    Added functionality for specimens

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/JustTetsing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/JustTetsing.java
@@ -6,6 +6,7 @@ import static org.firstinspires.ftc.teamcode.base.teleop.TeleOpComponents.extend
 import static org.firstinspires.ftc.teamcode.base.teleop.TeleOpComponents.innerClawPitch;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -15,19 +16,36 @@ import org.firstinspires.ftc.teamcode.base.teleop.TeleOpActions.TeleOpSequential
 import org.firstinspires.ftc.teamcode.base.teleop.TeleOpActions.TeleOpParallelAction;
 import org.firstinspires.ftc.teamcode.base.teleop.TeleOpActions.UninterruptiblePressTrigger;
 import org.firstinspires.ftc.teamcode.base.teleop.TeleOpActions.TeleOpAction;
+import org.firstinspires.ftc.teamcode.base.teleop.TeleOpActions.TeleOpSleepAction;
 import org.firstinspires.ftc.teamcode.base.teleop.LambdaInterfaces.Condition;
 
 @TeleOp
 public class JustTetsing extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        UninterruptiblePressTrigger sequence = new UninterruptiblePressTrigger(
+                new Condition[]{()->(gamepad1.y)},
+                new TeleOpAction[]{
+                        new TeleOpSequentialAction(
+                            new TeleOpParallelAction(
+                                clawPitch.setPositionAction(clawPitch.getPos("hoverPosition")),
+                                new TeleOpSequentialAction(
+                                        new TeleOpSleepAction(0.1),
+                                        clawPitch.setPositionAction(clawPitch.getPos("pickUpPosition"))
+                                )
+                            ),
+                            clawFingers.setPositionAction(92)
+                        )
+                }
+        );
         TeleOpComponents.initializeMechanisms(hardwareMap,telemetry,new Pose2d(0,0,Math.toRadians(90)));
+        clawFingers.setPosition(clawFingers.getPos("openPosition"));
+        clawPitch.setPosition(clawPitch.getPos("pickUpPosition"));
+        innerClawPitch.setPosition(innerClawPitch.getPos("pickUpPosition"));
         waitForStart();
         TeleOpActions.runLoop(
                 this::opModeIsActive,
-                extendo.triggeredMoveToTargetAction(()->(gamepad1.a),400),
-                extendo.triggeredMoveToTargetAction(()->(gamepad1.b),800),
-                extendo.triggeredDynamicAction(()->(gamepad1.right_trigger>0),()->(gamepad1.left_trigger>0),20),
+                sequence,
                 new UpdateTelemetryAction()
         );
     }
