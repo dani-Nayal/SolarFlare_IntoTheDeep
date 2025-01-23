aceb2adacf92cd61e27842ecc4f297637ab203b2
roobootics <arick@solarflarerobotics.org>
2025-01-22 19:33:11 -0500

    Added functionality for specimens

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/SkippyWippyUwU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/SkippyWippyUwU.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.base.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@TeleOp
 public class SkippyWippyUwU extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
