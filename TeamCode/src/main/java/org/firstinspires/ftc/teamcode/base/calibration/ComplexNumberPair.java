062c2ba780e66327f4810cbd21ef2bf22ee8b735
MuradGitHub <github@nayal.org>
2025-01-23 19:40:51 -0500

    Update ComplexNumberPair.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/ComplexNumberPair.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/ComplexNumberPair.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.base.calibration;
 
+import androidx.annotation.NonNull;
+
+import java.util.Locale;
+
 public class ComplexNumberPair {
     public ComplexNumber n1;
     public ComplexNumber n2;
@@ -7,5 +11,11 @@ public class ComplexNumberPair {
     ComplexNumberPair(ComplexNumber n1, ComplexNumber n2) {
         this.n1 = n1;
         this.n2 = n2;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "[%1$s, %2$s]", n1, n2);
     }
 }
