66fce46b6aaaa29bad8c9494f4e5dd72c45f9e64
roobootics <arick@solarflarerobotics.org>
2025-01-25 17:40:50 -0500

    Added functionality for specimens

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/TeleOpComponents.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/teleop/TeleOpComponents.java
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.hardware.ServoImpl;
@@ -56,6 +57,7 @@ public abstract class  TeleOpComponents {
 
     public static CRBotServo hang;
     public static CRBotServo hangRight;
+    public static BotMotor ryanNemesis;
 
     public static class BotMotor extends DcMotorImplEx {
         boolean isProfilePending = false; int profileDelayCounter = 1; int profileDelayFactor = 5;
@@ -409,6 +411,8 @@ public abstract class  TeleOpComponents {
                     accelDistance=0;
                     cruiseDistance=0;
                     decelDistance=0;
+                    telemetry.addData("e","e");
+                    telemetry.update();
                 }
             }
             else{
@@ -825,6 +829,21 @@ public abstract class  TeleOpComponents {
     public static void initializeMechanisms(HardwareMap hardwareMap, Telemetry telemetry, Pose2d initialDrivePose){
         TeleOpComponents.hardwareMap=hardwareMap;
         TeleOpComponents.telemetry=telemetry;
+        /*
+        ryanNemesis = new BotMotor(
+                hardwareMap.get(DcMotorEx.class, "motor-1").getController(),
+                hardwareMap.get(DcMotorEx.class, "motor-1").getPortNumber(),
+                hardwareMap.get(DcMotorEx.class, "motor-1").getMotorType(),
+                0.015,0,0.00002,
+                new String[]{},new double[]{},
+                Double.POSITIVE_INFINITY,Double.NEGATIVE_INFINITY,
+                250000,3500,
+                DcMotorEx.RunMode.RUN_WITHOUT_ENCODER,
+                DcMotorEx.Direction.FORWARD,
+                DcMotorEx.ZeroPowerBehavior.BRAKE,
+                "MOTION_PROFILE"
+        );
+        */
         TeleOpComponents.drive = new PinpointDrive(hardwareMap,initialDrivePose);
         //initialize mechanism variables here
         extendo = new BotMotor(
@@ -986,8 +1005,8 @@ public abstract class  TeleOpComponents {
                 Servo.Direction.FORWARD
         );
         synchronizeServos(clawPitch,clawPitchRight);
-    }
 
+    }
     public static void synchronizeServos(BotServo servo1, BotServo servo2){
         servo1.synchronizedServos.add(servo2);
     }
