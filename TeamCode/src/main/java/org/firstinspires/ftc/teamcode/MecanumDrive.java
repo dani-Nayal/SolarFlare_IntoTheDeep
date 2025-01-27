2fa7ed355f669f87480bedcdc26b292e4dde3801
Tristan Li <tristan@solarflarerobotics.org>
2025-01-26 23:34:44 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -71,7 +71,7 @@ public class MecanumDrive {
         public double kA = 0.01;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 30;
+        public double maxWheelVel = 50;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 60;
 
