de05f929513c3047cd85bfcfd069c16c06bd5c93
MuradGitHub <github@nayal.org>
2025-01-28 18:56:34 -0500

    Move Rig1MotorCalib to the calibration package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/Rig1MotorCalib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/Rig1MotorCalib.java
@@ -27,7 +27,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.firstinspires.ftc.teamcode.base.testing;
+package org.firstinspires.ftc.teamcode.base.calibration;
 
 import static java.util.logging.Level.INFO;
 
@@ -36,7 +36,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.base.calibration.MotorProfileConstP;
 import org.firstinspires.ftc.teamcode.base.config.HardwareConfig;
 import org.firstinspires.ftc.teamcode.base.config.MotorConfig;
 import org.firstinspires.ftc.teamcode.base.config.MotorEnum;
