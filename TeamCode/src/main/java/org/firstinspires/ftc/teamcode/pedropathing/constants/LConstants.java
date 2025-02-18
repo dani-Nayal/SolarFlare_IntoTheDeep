5559b64ff7d458b3e2d531fc070ce02fb3b4c117
dani-Nayal <dani.nayal@icloud.com>
2025-02-18 17:25:05 -0500

    odo offsets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/constants/LConstants.java
@@ -7,8 +7,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        PinpointConstants.forwardY = 1;
-        PinpointConstants.strafeX = -2.5;
+        PinpointConstants.forwardY = 3.925;
+        PinpointConstants.strafeX = 2.93;
         PinpointConstants.distanceUnit = DistanceUnit.INCH;
         PinpointConstants.hardwareMapName = "pinpoint";
         PinpointConstants.useYawScalar = false;
